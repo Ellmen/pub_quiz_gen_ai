94bd93f5949f8d34183f41266b3ba9798ab71cbd
Isaac Ellmen <isaacellmen@gmail.com>
2024-11-05 16:15:34 +0000

    Fixed prompt interface for Instruct

--- run_prompts.py
+++ run_prompts.py
@@ -16,32 +16,42 @@ descs = get_descs()
 finish_reason = 'blah'
 for name in descs:
     print(name)
-    # prompt = f"""{descs[name]}
-
-    # Please summarize the above in the form of a haiku. Just give me the haiku and nothing else. Finish after 3 lines.
-    # """
     prompt = f"""{descs[name]}
 
-    Please summarize the above in exactly one brief sentence using 10 words or less. Stop after one sentence.
+    Please summarize the above in the form of a haiku. Just give me the haiku and nothing else. Finish after 3 lines.
     """
+    # prompt = f"""{descs[name]}
+
+    # The above is a research description. Please summarize what this researcher does differently compared to other researchers in data driven drug designin exactly one brief sentence using 10 words or less.
+    # """
 
     while finish_reason != 'stop':
-        completion = client.completions.create(
+        # completion = client.completions.create(
+        #     model="meta-llama/Llama-3.2-3B-Instruct",
+        #     prompt=prompt,
+        #     max_tokens=40,
+        #     # temperature=0.2,
+        #     # top_p=0.2,
+        # )
+        completion = client.chat.completions.create(
             model="meta-llama/Llama-3.2-3B-Instruct",
-            prompt=prompt,
-            max_tokens=40,
-            # temperature=0.2,
-            # top_p=0.2,
+            messages=[{'role': 'user', 'content': prompt}],
+            max_tokens=100,
+            temperature=0.6,
+            top_p=0.9,
+            stream=False,
         )
         finish_reason = completion.choices[0].finish_reason
         print(finish_reason)
-        poem = completion.choices[0].text
+        poem = completion.choices[0].message.content
+        # poem = completion.choices[0].text
         print(poem)
         # print(prompt, completion.choices[0].text)
     finish_reason = 'blah'
-    poem = completion.choices[0].text
+    # poem = completion.choices[0].text
+    poem = completion.choices[0].message.content
     print(poem)
     with open(f'run3/{name}.txt', 'w') as f:
         f.write(poem)
-    # print(prompt, completion.choices[0])
+    print(prompt, completion.choices[0])
 
