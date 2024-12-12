0b45b59fcfbe283fcc324104606adc5d7abcff5d
Isaac Ellmen <isaacellmen@gmail.com>
2024-12-12 15:21:56 +0000

    Trimmed down

--- run_prompts.py
+++ run_prompts.py
@@ -9,9 +9,6 @@ client = OpenAI(
     base_url=openai_api_base,
 )
 
-# A one sentence summary of the above for toddlers is:
-# A summary of the above in the form of a haiku is:
-
 descs = get_descs()
 finish_reason = 'blah'
 for name in descs:
@@ -20,19 +17,8 @@ for name in descs:
 
     Please summarize the above in the form of a haiku. Just give me the haiku and nothing else. Finish after 3 lines.
     """
-    # prompt = f"""{descs[name]}
-
-    # The above is a research description. Please summarize what this researcher does differently compared to other researchers in data driven drug designin exactly one brief sentence using 10 words or less.
-    # """
 
     while finish_reason != 'stop':
-        # completion = client.completions.create(
-        #     model="meta-llama/Llama-3.2-3B-Instruct",
-        #     prompt=prompt,
-        #     max_tokens=40,
-        #     # temperature=0.2,
-        #     # top_p=0.2,
-        # )
         completion = client.chat.completions.create(
             model="meta-llama/Llama-3.2-3B-Instruct",
             messages=[{'role': 'user', 'content': prompt}],
@@ -41,14 +27,8 @@ for name in descs:
             top_p=0.9,
             stream=False,
         )
-        finish_reason = completion.choices[0].finish_reason
-        print(finish_reason)
         poem = completion.choices[0].message.content
-        # poem = completion.choices[0].text
         print(poem)
-        # print(prompt, completion.choices[0].text)
-    finish_reason = 'blah'
-    # poem = completion.choices[0].text
     poem = completion.choices[0].message.content
     print(poem)
     with open(f'run3/{name}.txt', 'w') as f:
